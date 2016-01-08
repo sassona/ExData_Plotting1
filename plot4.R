e3b7cd3529a7d0e5e554fb6fc276e067c0677571
sassona <ariella.sasson@gmail.com>
2016-01-08 15:34:07 -0500

    plots and code5

--- plot4.R
+++ plot4.R
@@ -20,12 +20,28 @@ DateTime<-strptime(x, "%d/%m/%Y %H:%M:%S")
 household_fixed<-cbind(DateTime,household)           
 
 #plot generation
-png("plot3.png",width = 480, height = 480)
+png("plot4.png",width = 480, height = 480)
+par(mfcol=c(2,2))
+
+#plot 1
+plot(household_fixed$DateTime,household_fixed$Global_active_power,type = "l",
+     ylab ="Global Active Power",xlab="")
+
+#plot 2
 plot(household_fixed$DateTime,household_fixed$Sub_metering_1,type = "n",
         ylab ="Energy sub metering",xlab="")
 lines(household_fixed$DateTime,household_fixed$Sub_metering_1)
 lines(household_fixed$DateTime,household_fixed$Sub_metering_2,col="red")
 lines(household_fixed$DateTime,household_fixed$Sub_metering_3,col="blue")
-legend("topright",col=c("black","red","blue"),c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1))
+legend("topright",col=c("black","red","blue"),c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),bty="n")
+
+#plot 3
+plot(household_fixed$DateTime,household_fixed$Voltage,type = "l",
+     ylab ="Voltage",xlab="datetime")
+
+#plot 4
+plot(household_fixed$DateTime,household_fixed$Global_reactive_power,type = "l",
+     ylab ="Global_reactive_power",xlab="datetime")
+
 dev.off()
 
